5d0b9dd8b17663ed72bc7dd1073b6ec1ddf4b71f
Tan Nguyen <tannp27@gmail.com>
2022-08-16 14:06:37 +0700

    set key for modules

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Conversation extends Model
 {
     use HasFactory;
+
+    public function messages() {
+        return $this->hasMany(Message::class);
+    }
+
+    public function conversationUsers() {
+        return $this->hasMany(ConversationUser::class);
+    }
 }
--- app/Models/ConversationUser.php
+++ app/Models/ConversationUser.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class ConversationUser extends Model
 {
     use HasFactory;
+
+    public function conversations() {
+        return $this->belongsTo(Conversation::class);
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class, 'conversation_users', 'conversation_id', 'user_id',
+            'message_id', 'sender_id')
+            ->withTimestamps();
+    }
 }
